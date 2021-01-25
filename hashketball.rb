2aea1528b1ad12e15a5a0ff09daa6eef81bd053b
Nick Gray <nickgray87@gmail.com>
2021-01-25 22:22:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,21 +2,31 @@ require 'pry'
 
 def game_hash
   {
-    :home => {
-      :team_name => "Brooklyn Nets",
-      :colors => "black, white",
-      :players => [ {
-        :player_name
-        :number
-        :shoe
-        :points
-        :rebounds
-        :assists
-        :steals
-        :blocks
-        :slam_dunks
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: "Black, White",
+      players: {
+        "Alan Anderson" => {
+          number: 0, 
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3
+          blocks:
+          slam_dunks:
+        },
+        "Reggie Evans" => {
+          number: ,
+          shoe: ,
+          points: ,
+          rebounds: ,
+          assists: ,
+          steals: ,
+          blocks: ,
+          slam_dunks: ,
+        },
       }
-        ]
       
     }
     :away => {
