7e2a799d558704614b47e376fe7d9cebfa26a51c
Nick Gray <nickgray87@gmail.com>
2021-01-25 22:16:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,39 @@
-# Write your code here!
+require 'pry'
+
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => "black, white",
+      :players => [ {
+        :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+      }
+        ]
+      
+    }
+    :away => {
+      :team_name =>
+      :colors =>
+      :players =>[ {
+        :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks 
+        
+      }
+      ]
+    }
+  }
